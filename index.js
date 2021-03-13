3c764c0297166baa72e7472b94ba8a6300b24491
OderoOluoch <oderoluoch@gmail.com>
2021-03-13 15:44:34 +0300

    we now transfer the details in the array into the variable a,b,c,d

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 totalProduction(){
-    //create an array variable to store the shades
+    //create an array variable to store the sales per shade in one day
     var shades = [];
 
     shades.push(document.getElementById("shade_a").value);
+    shades.push(document.getElementById("shade_b").value);
+    shades.push(document.getElementById("shade_c").value);
+    shades.push(document.getElementById("shade_d").value);
+
+    var [a,b,c,d] = shades;
 }
