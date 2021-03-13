b83d59f50f564268a48bd0dcd407dff801ed02a6
OderoOluoch <oderoluoch@gmail.com>
2021-03-13 17:19:31 +0300

    Trial and error on the css form and js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-totalProduction(){
+var totalProduction = function(){
     //create an array variable to store the sales per shade in one day
     var shades = [];
 
@@ -12,18 +12,27 @@ totalProduction(){
     var [a,b,c,d] = shades;
 
     var displayShadesSales = document.getElementById("display_shades_sales");
-    if(a.length === 0 || b.length === 0 || c.length || d.length){
+    if(a.length === 0 || b.length === 0 || c.length === 0 || d.length === 0){
         displayShadesSales.innerHTML = "<p>Please fill in the values to calculate sales</p>"
     }else{
     //we get to convert the user input from string variables, to number variables
         a = parseFloat(a);
         b = parseFloat(b);
-        b = parseFloat(c);
-        c = parseFloat(d);
-        totalProductionCalculation()
+        c = parseFloat(c);
+        d = parseFloat(d);
+        totalProductionCalculation(a,b,c,d)
     }
 }
 
-totalProductionCalculation(){
-    console.log("TUko hapa");
+var totalProductionCalculation = function(a,b,c,d){
+    var total = a + b + c + d;
+    console.log("Your production in Shed A " + a + " litres per day ")
+    console.log("Your production in Shed B " + b + " litres per day ")
+    console.log("Your production in Shed C " + c + " litres per day ")
+    console.log("Your production in Shed D " + d + " litres per day ")
+    console.log("The total production is " +total + " litres per day")
 }
+
+var reset = function(){
+    
+} 
